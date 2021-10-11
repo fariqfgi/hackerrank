03fb2fef5f2bc1a9004d8b4a15f442e2a96b9fd1
fariqfgi <fariq.fgi@gmail.com>
2021-10-11 07:53:39 +0700

    Add: Simple Array Sum

---
+++ simple-array-sum.php
@@ -0,0 +1,18 @@
+<?php
+
+function simpleArraySum($ar)
+{
+ 
+    $result = 0;
+
+    foreach ($ar as $value)
+    {
+        $result += $value;
+    }
+
+    return $result;
+}
+
+$ar = array(1, 4, 7);
+
+var_dump(simpleArraySum($ar));
